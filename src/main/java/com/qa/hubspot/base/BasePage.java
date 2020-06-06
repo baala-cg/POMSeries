44352cec411584da8ad0c2e972e62d1a4f88fca2
JyothiKrishna@LAPTOP-6SA14LGI.(none) <baala.cg@gmail.com>
2020-06-06 13:56:44 +0530

    POM Series Initial Commit

---
+++ src/main/java/com/qa/hubspot/base/BasePage.java
@@ -0,0 +1,136 @@
+package com.qa.hubspot.base;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import com.qa.hubspot.utils.ElementUtil;
+import com.qa.hubspot.utils.OptionsManager;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * 
+ * @author NaveenKhunteta
+ *
+ */
+public class BasePage {
+
+	WebDriver driver;
+	public Properties prop;
+	public ElementUtil elementUtil;
+	public OptionsManager optionsManager;
+
+	public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<WebDriver>();
+
+	public static synchronized WebDriver getDriver() {
+		return tlDriver.get();
+	}
+
+	/**
+	 * this method is used to initialize the WebDriver on the basis of browser
+	 * 
+	 * @param browserName
+	 * @return driver
+	 */
+	public WebDriver init_driver(Properties prop) {
+		optionsManager = new OptionsManager(prop);
+
+		String browserName = prop.getProperty("browser");
+
+		if (browserName.equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			tlDriver.set(new ChromeDriver(optionsManager.getChromeOptions()));
+
+		} else if (browserName.equalsIgnoreCase("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			tlDriver.set(new FirefoxDriver(optionsManager.getFirefoxOptions()));
+
+		} else if (browserName.equalsIgnoreCase("safari")) {
+			WebDriverManager.getInstance(SafariDriver.class).setup();
+			tlDriver.set(new SafariDriver());
+		}
+
+		getDriver().manage().deleteAllCookies();
+		getDriver().manage().window().fullscreen();
+
+		getDriver().get(prop.getProperty("url"));
+
+		return getDriver();
+
+	}
+
+	/**
+	 * this method is used to initialize the properties from config.proeprties
+	 * file on the basis of given env variable
+	 * 
+	 * 
+	 * @return prop
+	 */
+	public Properties init_prop() {
+		prop = new Properties();
+		String path = null;
+		String env = null;
+
+		try {
+			env = System.getProperty("env");
+			System.out.println("env value is--->" + env);
+			if (env == null) {
+				path = "./src/main/java/com/qa/hubspot/config/config.properties";
+			} else {
+				switch (env) {
+				case "qa":
+					path = "./src/main/java/com/qa/hubspot/config/qa.config.properties";
+					break;
+				case "dev":
+					path = "./src/main/java/com/qa/hubspot/config/dev.config.properties";
+					break;
+				case "stage":
+					path = "./src/main/java/com/qa/hubspot/config/stage.config.properties";
+					break;
+				default:
+					System.out.println("Please pass the correct env value----> " + env);
+					break;
+				}
+			}
+
+			FileInputStream ip = new FileInputStream(path);
+			prop.load(ip);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return prop;
+	}
+
+	/**
+	 * this method will take the screenshot
+	 */
+	public String getScreenshot() {
+
+		File src = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
+		String path = System.getProperty("user.dir") + "/screenshots/" + System.currentTimeMillis() + ".png";
+		File destination = new File(path);
+		try {
+			FileUtils.copyFile(src, destination);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return path;
+
+	}
+
+}
